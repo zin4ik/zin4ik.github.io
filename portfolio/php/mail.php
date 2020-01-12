8811a43568950e049939c52508ac5d95e37658fa
zin4ik <43877261+zin4ik@users.noreply.github.com>
2020-01-12 22:15:58 +0200

    php

--- portfolio/php/mail.php
+++ portfolio/php/mail.php
@@ -2,7 +2,7 @@
 $recepiant = "zin4ik@i.ua";
 $sitename = "my portfolio";
 
-$name = trim($_POST['name'];
+$name = trim($_POST['name']);
 $phone = trim($_POST['phone']);
 $email = trim($_POST['email']);
 $text = trim($_POST['text']);
@@ -10,7 +10,7 @@ $message="Імя: $name,\n Телефон: $phone,\n Email: $email, \n $text";
 $pageTitle="Заявка з сайта $sitename";
 
 $headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
-$headers .= "From: От кого письмо <from@example.com>\r\n"; 
-$headers .= "Reply-To: reply-to@example.com\r\n"; 
+// $headers .= "From: От кого письмо <from@example.com>\r\n"; 
+// $headers .= "Reply-To: reply-to@example.com\r\n"; 
 mail($recepiant,$sitename,$message,$headers);
 ?>

1190cafb8150b54a84fee3598e430cabe60877f8
zin4ik <43877261+zin4ik@users.noreply.github.com>
2020-01-26 18:01:33 +0200

    er

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -106,16 +106,16 @@ jQuery(document).ready(function() {
   //   var el = $(this).attr("value");
   $("#lang").change(function() {
     // alert(this.value);
-    var lang=this.value
+    var lang = this.value;
     alert(lang);
-    if (land == "uk") {
+    if ((land = "uk")) {
       $("uk").css("display", "block");
       $("en").css("display", "none");
-      alert('lang uk');
+      alert("lang uk");
     } else {
       $("uk").css("display", "none");
       $("en").css("display", "block");
-      alert('lang en');
+      alert("lang en");
     }
 
     // console.log(el);

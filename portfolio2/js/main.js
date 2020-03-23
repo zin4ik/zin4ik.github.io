9d5d1cf88c1269d552e2403d0d877e2ea0272b32
zin4ik <43877261+zin4ik@users.noreply.github.com>
2020-03-23 23:04:45 +0200

    f com

---
+++ portfolio2/js/main.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+  $(".variable-width").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    autoplay: true,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $(".blog__card").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    vertical: true,
+    autoplay: false,
+    prevArrow: $('.card-prev'),
+    nextArrow: $('.card-next'),
+    verticalSwiping: true
+  });
+  //text-overflow
+  function cropTitle() {
+    $(".blog__card-block_descr").each(function() {
+      var $title = $(this).find("p");
+      while ($title.height() > $(this).height()) {
+        $title.text(
+          $title
+            .text()
+            .split(" ")
+            .slice(0, $title.text().split(" ").length - 1)
+            .join(" ") + "..."
+        );
+      }
+    });
+  }
+  cropTitle();
+
+  //--/text-overflow
+});

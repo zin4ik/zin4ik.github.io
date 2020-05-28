8a25108ccfde4f910a265c23333336c1b945060f
zin4ik <43877261+zin4ik@users.noreply.github.com>
2020-05-28 21:48:47 +0300

    add animete

--- creativia/js/main.js
+++ creativia/js/main.js
@@ -1,52 +1,81 @@
-jQuery(document).ready(function($) {
- // menu-active
+jQuery(document).ready(function ($) {
+  new WOW().init();
+  //nav menu
   // Cache selectors
-var lastId,
-topMenu = $("#top-menu"),
-topMenuHeight = topMenu.outerHeight()+15,
-// All list items
-menuItems = topMenu.find("a"),
-// Anchors corresponding to menu items
-scrollItems = menuItems.map(function(){
-  var item = $($(this).attr("href"));
-  if (item.length) { return item; }
-});
+  var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+      var item = $($(this).attr("href"));
+      if (item.length) {
+        return item;
+      }
+    });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-var href = $(this).attr("href"),
-  offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 1000);
-  e.preventDefault();
-});
+  // Bind click handler to menu items
+  // so we can get a fancy scroll animation
+  menuItems.click(function (e) {
+    var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $("html, body").stop().animate(
+      {
+        scrollTop: offsetTop,
+      },
+      1000
+    );
+    e.preventDefault();
+  });
 
-// Bind to scroll
-$(window).scroll(function(){
-// Get container scroll position
-var fromTop = $(this).scrollTop()+topMenuHeight;
+  // Bind to scroll
+  $(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
 
-// Get id of current scroll item
-var cur = scrollItems.map(function(){
- if ($(this).offset().top < fromTop)
-   return this;
-});
-// Get the id of the current element
-cur = cur[cur.length-1];
-var id = cur && cur.length ? cur[0].id : "";
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+      if ($(this).offset().top < fromTop) return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
 
-if (lastId !== id) {
-   lastId = id;
-   // Set/remove active class
-   menuItems.addClass('text-white-50').parent().removeClass("active")
-     .end().filter("[href='#"+id+"' ]").removeClass('text-white-50').parent().addClass("active");
-}                   
-});
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .addClass("text-white-50")
+        .parent()
+        .removeClass("active-menu")
+        .end()
+        .filter("[href='#" + id + "' ]")
+        .removeClass("text-white-50")
+        .parent()
+        .addClass("active-menu");
+    }
+  });
 
-       
-//slick slider
+  //click collapse
+
+  $(".accordion__btn").on("click", function () {
+    var btn = $(this);
+    var id = btn.attr("data-target");
+    console.log($(id));
+    $(id).on("hidden.bs.collapse", function () {
+      console.log("click3");
+      btn.children(".fa-minus").css("opacity", "0");
+      btn.children(".fa-plus").css("opacity", "1");
+    });
+    $(id).on("shown.bs.collapse", function () {
+      console.log("click2");
+      btn.children(".fa-minus").css("opacity", "1");
+      btn.children(".fa-plus").css("opacity", "0");
+    });
+  });
+
+  //slick slider
   $(".variable-width").slick({
     dots: false,
     arrows: false,
@@ -63,8 +92,8 @@ if (lastId !== id) {
           arrows: false,
           centerMode: true,
           centerPadding: "40px",
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 480,
@@ -72,10 +101,10 @@ if (lastId !== id) {
           arrows: false,
           centerMode: true,
           centerPadding: "40px",
-          slidesToShow: 1
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".blog__card").slick({
     slidesToShow: 2,
@@ -83,13 +112,13 @@ if (lastId !== id) {
     arrows: true,
     vertical: true,
     autoplay: false,
-    prevArrow: $('.card-prev'),
-    nextArrow: $('.card-next'),
-    verticalSwiping: true
+    prevArrow: $(".card-prev"),
+    nextArrow: $(".card-next"),
+    verticalSwiping: true,
   });
   //text-overflow
   function cropTitle() {
-    $(".blog__card-block_descr").each(function() {
+    $(".blog__card-block_descr").each(function () {
       var $title = $(this).find("p");
       while ($title.height() > $(this).height()) {
         $title.text(
@@ -105,8 +134,9 @@ if (lastId !== id) {
   cropTitle();
 
   //--/text-overflow
-   //send masege
-  $(".form-block_title").on("click", function() {
+
+  //send masege
+  $(".form-block_title").on("click", function () {
     var formBlock = $(".form-block");
     if (formBlock.hasClass("form-block-active")) {
       formBlock.removeClass("form-block-active");

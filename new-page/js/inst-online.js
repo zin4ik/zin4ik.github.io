e7636750650e53cf2bfaa7bfc0382ef8bb975c68
zin4ik <43877261+zin4ik@users.noreply.github.com>
2021-01-29 00:17:27 +0200

    7comm

--- new-page/js/inst-online.js
+++ new-page/js/inst-online.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let title = document.getElementById('title-radius')
+    let title = document.getElementById('title-radius');
     if(title){
         new CircleType(title);
     }
+    // const player = new Plyr('#player1');
+    // const player = new Plyr('#player2');
+    // const player = new Plyr('#player3');
 
 });
+
+(function($){
+    $(document).ready(function(){
+        $('.btn-play').on('click',function(){
+        var popup = $(this).data('playerpopup');
+        $('#'+popup).css('display','block');
+           
+        })
+        $('.inst-player-popup-close').on('click',function(){
+            $(this).parent('.inst-player-popup').hide();
+        })
+    })
+
+})(jQuery);

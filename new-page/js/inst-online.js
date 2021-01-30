3716cdda24c6c7abe2a453a753af417e2606919e
zin4ik <43877261+zin4ik@users.noreply.github.com>
2021-01-30 16:41:11 +0200

    8comm

--- new-page/js/inst-online.js
+++ new-page/js/inst-online.js
@@ -3,20 +3,44 @@ document.addEventListener("DOMContentLoaded", ()=>{
     if(title){
         new CircleType(title);
     }
-    // const player = new Plyr('#player1');
-    // const player = new Plyr('#player2');
-    // const player = new Plyr('#player3');
 
 });
 
 (function($){
     $(document).ready(function(){
+
+        $('.circle-text').css('opacity','1');
+        var player = new Plyr('#player');
+        var player2 = new Plyr('#player2');
+        var player3 = new Plyr('#player3');
+         
+
+
+
         $('.btn-play').on('click',function(){
-        var popup = $(this).data('playerpopup');
-        $('#'+popup).css('display','block');
-           
+            var popup = $(this).data('playerpopup');
+            $('#'+popup).css('display','block');
+            var id = $('#'+popup).find('video').attr('id');
+            if( id == 'player'){
+                player.play();
+            }else if(id == 'player2'){
+                player2.play();
+            }
+            else if(id == 'player3'){
+                player3.play();
+            }
+
         })
         $('.inst-player-popup-close').on('click',function(){
+            var idpLayer = $(this).parent().find('video').attr('id');
+            if( idpLayer  == 'player'){
+                player.stop();
+            }else if(idpLayer == 'player2'){
+                player2.stop();
+            }
+            else if(idpLayer == 'player3'){
+                player3.stop();
+            }
             $(this).parent('.inst-player-popup').hide();
         })
     })
